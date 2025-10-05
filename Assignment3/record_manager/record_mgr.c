7d1025bffe74b9cfd44af7490a1da4a521fe5d0d
pipeking636 <pipeking@foxmail.com>
2025-10-05 22:41:44 +0800

    重建版本

--- Assignment3/record_manager/record_mgr.c
+++ Assignment3/record_manager/record_mgr.c
@@ -35,28 +35,6 @@ typedef struct RM_TableMgmt {
 } RM_TableMgmt;
 
 // debug functions
-// show schema
-static void showSchema(Schema *schema)
-{
-    if(schema == NULL)
-        return;
-    printf("Schema: %d attributes\n", schema->numAttr);
-    for(int i = 0; i < schema->numAttr; i++)
-    {
-        printf("  %s: %d", schema->attrNames[i], schema->typeLength[i]);
-        switch(schema->dataTypes[i])
-        {
-            case DT_INT: printf(" (int)"); break;
-            case DT_STRING: printf(" (string)"); break;
-            case DT_FLOAT: printf(" (float)"); break;
-            case DT_BOOL: printf(" (bool)"); break;
-            default: printf(" (unknown)"); break;
-        }
-        if(i < schema->keySize)
-            printf(" (key)");
-        printf("\n");
-    }
-}
 
 // table and manager
 /**
@@ -67,7 +45,6 @@ static void showSchema(Schema *schema)
  */
 RC initRecordManager (void *mgmtData)
 {
-    initStorageManager(); // 初始化存储管理器
     return RC_OK;
 }
 
@@ -89,46 +66,6 @@ RC shutdownRecordManager ()
  */
 RC createTable (char *name, Schema *schema)
 {
-    // 1. create page file
-    if(createPageFile(name) != RC_OK)
-        return RC_FILE_HANDLE_NOT_INIT;
-    // 2. initialize table info (page 0)
-    SM_FileHandle fh;
-    if(openPageFile(name, &fh) != RC_OK)
-        return RC_FILE_HANDLE_NOT_INIT;
-    // calculate record size
-    int recordSize = getRecordSize(schema);
-    if(recordSize <= 0)
-    {
-        closePageFile(&fh);
-        return RC_UNVALID_HANDLE;
-    }
-    // 3. initialize table info
-    TableInfo tableInfo;
-    strcpy(tableInfo.tableName, name); //table name keep normal string length and "\0" ending
-    tableInfo.schema = *schema; // copy schema, please note dynamic memory in schema
-    tableInfo.numTuples = 0; // no tuple in the table yet
-    tableInfo.totalPages = 1; // at least one page for table info
-    tableInfo.freePageListHead = -1; // no free page
-    tableInfo.recordSize = recordSize; // record size is fixed
-    // 4. write table info to page 0
-    SM_PageHandle page = (SM_PageHandle)calloc(PAGE_SIZE, sizeof(char));
-    // 5. set page 0 header
-    PageHeader phHeader;
-    phHeader.freeSpaceOffset = PAGE_SIZE - sizeof(PageHeader); // free space start from page tail
-    phHeader.slotCount = 0; // no slot used yet
-    phHeader.nextFreePage = -1; // no next free page
-    phHeader.isTableInfoPage = true; // mark as table info page
-    memcpy(page, &phHeader, sizeof(PageHeader));
-    memcpy(page + sizeof(PageHeader), &tableInfo, sizeof(TableInfo));
-    if (writeBlock(0, &fh, page) != RC_OK)
-    {
-        free(page);
-        closePageFile(&fh);
-        return RC_WRITE_FAILED;
-    }
-    free(page);
-    closePageFile(&fh);
     return RC_OK;
 }
 /**
@@ -140,56 +77,6 @@ RC createTable (char *name, Schema *schema)
  */
 RC openTable (RM_TableData *rel, char *name)
 {
-    RC ret = RC_OK;
-
-    if(rel == NULL || name == NULL)
-        return RC_UNVALID_HANDLE; 
-
-    // 1. initialize RM_TableData
-    rel->name = strdup(name); // copy table name, please note dynamic memory in string
-    rel->schema = (Schema *)malloc(sizeof(Schema)); // copy schema, please note dynamic memory in schema
-    rel->mgmtData =(RM_TableMgmt *)malloc(sizeof(RM_TableMgmt)); // copy mgmtData, please note dynamic memory in mgmtData
-    RM_TableMgmt *mgmt = (RM_TableMgmt *)rel->mgmtData; // cast mgmtData to RM_TableMgmt
-    ret=openPageFile(name, &mgmt->fileHandle);
-    if(ret != RC_OK)
-    {
-        free(rel->name);
-        free(rel->schema);
-        free(rel->mgmtData);
-        return ret;        
-    }
-    // 2. initialize buffer pool (use FIFO policy)
-    ret = initBufferPool(&mgmt->bufferPool, name, 10, RS_FIFO, NULL);
-    if ( ret != RC_OK)
-    {
-        free(rel->name);
-        free(rel->schema);
-        free(rel->mgmtData);
-        return ret;
-    }
-    // 3. read table info from page 0
-    BM_PageHandle ph;
-    ret = pinPage(&mgmt->bufferPool,&ph,0);
-    if( ret != RC_OK)
-    {
-        free(rel->name);
-        free(rel->schema);
-        free(rel->mgmtData);
-        return ret;
-    }
-    // copy table info to mgmt
-    TableInfo *tableInfo = (TableInfo *)ph.data;
-    mgmt->tableInfo = *tableInfo; // copy table info, please note dynamic memory in schema
-    *rel->schema = mgmt->tableInfo.schema; // copy schema, please note dynamic memory in schema
-    // 4. unpin page 0
-    ret = unpinPage(&mgmt->bufferPool, &ph);
-    if( ret != RC_OK)
-    {
-        free(rel->name);
-        free(rel->schema);
-        free(rel->mgmtData);
-        return ret;
-    }
     return RC_OK;
 }
 /**
@@ -200,27 +87,6 @@ RC openTable (RM_TableData *rel, char *name)
  */
 RC closeTable (RM_TableData *rel)
 {
-    RC ret = RC_OK;
-    if(rel == NULL || rel->mgmtData == NULL)
-        return RC_UNVALID_HANDLE;
-    RM_TableMgmt *mgmt = (RM_TableMgmt *)rel->mgmtData; // cast mgmtData to RM_TableMgmt
-    // 1. close buffer pool
-    ret = shutdownBufferPool(&mgmt->bufferPool);
-    if( ret != RC_OK)
-    {
-        /*maybe need more error handling*/
-        return ret;
-    }
-    ret = closePageFile(&mgmt->fileHandle);
-    if( ret != RC_OK) return ret;
-    
-    // 2. release table info
-    free(rel->name);
-    free(rel->schema);
-    free(rel->mgmtData);
-    rel->name = NULL;
-    rel->schema = NULL;
-    rel->mgmtData = NULL;
     return RC_OK;
 }
 /**
@@ -231,9 +97,7 @@ RC closeTable (RM_TableData *rel)
  */
 RC deleteTable (char *name)
 {
-    if(name == NULL)
-        return RC_UNVALID_HANDLE;
-    return destroyPageFile(name);
+    return RC_OK;
 }
 /**
  * @brief get number of tuples in a table
@@ -243,10 +107,7 @@ RC deleteTable (char *name)
  */
 int getNumTuples (RM_TableData *rel)
 {
-    if(rel == NULL || rel->mgmtData == NULL)
-        return RC_UNVALID_HANDLE;
-    RM_TableMgmt *mgmt = (RM_TableMgmt *)rel->mgmtData; // cast mgmtData to RM_TableMgmt
-    return mgmt->tableInfo.numTuples;
+    return 0;
 }
 
 // handling records in a table
@@ -259,75 +120,6 @@ int getNumTuples (RM_TableData *rel)
  */
 RC insertRecord (RM_TableData *rel, Record *record)
 {
-    RC ret = RC_OK;
-
-    if(rel == NULL || rel->mgmtData == NULL || record == NULL)
-        return RC_UNVALID_HANDLE;
-
-    // get record size
-    RM_TableMgmt *mgmt = (RM_TableMgmt *)rel->mgmtData; // cast mgmtData to RM_TableMgmt
-    int recordSize = getRecordSize(rel->schema);
-    if(recordSize<=0) return RC_RM_INVALID_RECORD_SIZE;
-
-    // 1. find valid page(from free list first)
-    int targetPageNum =-1;
-    PageHeader phHeader;
-    BM_PageHandle ph;
-
-    // traverse free list to find enough free space
-    int currPage = mgmt->tableInfo.freePageListHead;
-    while(currPage != -1)
-    {
-        // pin current free page, read page header
-        ret = pinPage(&mgmt->bufferPool, &ph, currPage);
-        if( ret != RC_OK) 
-        {
-            currPage = -1;
-            break;
-        }
-        // parse page header
-        memcpy(&phHeader, ph.data, sizeof(PageHeader));
-        // check free space (page size-header size-slot size-record size)
-        int freeSpace = phHeader.freeSpaceOffset - sizeof(PageHeader) - phHeader.slotCount * recordSize;
-        if (freeSpace >= recordSize) {
-            targetPageNum = currPage;
-            break;  // find free page with enough space
-        } else {
-            // this page no enough space, unpin and move to next free page
-            unpinPage(&mgmt->bufferPool, &ph);
-            currPage = phHeader.nextFreePage;
-        }
-    }
-    // 2. if can not find free page, create new page
-    if(targetPageNum == -1)
-    {
-        ret = appendEmptyBlock(&mgmt->fileHandle);
-        if( ret != RC_OK) return ret;
-        targetPageNum = mgmt->fileHandle.totalNumPages - 1; // new page number
-        // pin new page, and initialize page header
-        ret = pinPage(&mgmt->bufferPool, &ph, targetPageNum);
-        if( ret != RC_OK) return ret;
-        // initialize page header
-        phHeader.freeSpaceOffset = PAGE_SIZE; // free space offset is end of page
-        phHeader.slotCount = 0;
-        phHeader.nextFreePage = mgmt->tableInfo.freePageListHead; // link to free list
-        mgmt->tableInfo.freePageListHead = targetPageNum; // update free list head
-        memcpy(ph.data, &phHeader, sizeof(PageHeader));
-        markDirty(&mgmt->bufferPool, &ph);// mark page as dirty
-    }
-    // 3. insert record to page
-    // calculate record offset(page header size + slot count * record size)
-    int recordOffset = sizeof(PageHeader) + phHeader.slotCount * recordSize;
-    // copy record data to page
-    memcpy(ph.data + recordOffset, record->data, recordSize);
-    // update page header
-    phHeader.slotCount++;
-    phHeader.freeSpaceOffset -= recordSize; // update free space offset
-    memcpy(ph.data, &phHeader, sizeof(PageHeader));
-    // mark page as dirty
-    markDirty(&mgmt->bufferPool, &ph);// mark page as dirty
-    unpinPage(&mgmt->bufferPool, &ph); // unpin page
-
     return RC_OK;
 }
 /**
@@ -340,30 +132,6 @@ RC insertRecord (RM_TableData *rel, Record *record)
  */
 RC getRecord (RM_TableData *rel, RID id, Record *record)
 {
-    RC ret;
-    if(rel == NULL || rel->mgmtData == NULL || record == NULL)
-        return RC_UNVALID_HANDLE;
-
-    RM_TableMgmt *mgmt = (RM_TableMgmt *)rel->mgmtData; // cast mgmtData to RM_TableMgmt
-    int recordSize = mgmt->tableInfo.recordSize;
-    // 1. pin page
-    BM_PageHandle ph;
-    ret = pinPage(&mgmt->bufferPool, &ph, id.page);
-    if( ret != RC_OK) return ret;
-    // 2. calculate data offset
-    PageHeader *pageHeader = (PageHeader *)ph.data;
-    int dataOffset = sizeof(PageHeader) + id.slot * recordSize;
-    if(dataOffset + recordSize > pageHeader->freeSpaceOffset)
-    {
-        unpinPage(&mgmt->bufferPool, &ph);
-        return RC_RM_NO_MORE_TUPLES;
-    }
-    // 3. copy record to record
-    memcpy(record->data, ph.data + dataOffset, recordSize);
-    // 4. set RID
-    record->id = id;
-    ret = unpinPage(&mgmt->bufferPool, &ph);
-    if( ret != RC_OK) return ret;   
     return RC_OK;
 }
 RC deleteRecord (RM_TableData *rel, RID id)
@@ -396,21 +164,7 @@ RC closeScan (RM_ScanHandle *scan)
  */
 int getRecordSize (Schema *schema)
 {
-    if(schema == NULL)
-        return RC_UNVALID_HANDLE;
-    int size = 0;
-    for(int i = 0; i < schema->numAttr; i++)
-    {
-        switch(schema->dataTypes[i])
-        {
-            case DT_INT: size += sizeof(int); break;
-            case DT_STRING: size += schema->typeLength[i]; break;
-            case DT_FLOAT: size += sizeof(float); break;
-            case DT_BOOL: size += sizeof(bool); break;
-            default: return RC_UNVALID_HANDLE;
-        }
-    }
-    return size;
+    return 0;
 }
 
 /**
@@ -426,20 +180,7 @@ int getRecordSize (Schema *schema)
  */
 Schema *createSchema (int numAttr, char **attrNames, DataType *dataTypes, int *typeLength, int keySize, int *keys)
 {
-    if(numAttr <= 0 || attrNames == NULL || dataTypes == NULL || typeLength == NULL || keySize < 0 || keys == NULL)
-        return NULL;
-    Schema *schema = (Schema *)malloc(sizeof(Schema));
-    if(schema == NULL)
-        return NULL;
-    schema->numAttr = numAttr;
-    schema->attrNames = attrNames;
-    schema->dataTypes = dataTypes;
-    schema->typeLength = typeLength;
-    schema->keySize = keySize;
-    schema->keyAttrs = keys;
-
-    showSchema(schema);
-    return schema;
+    return NULL;
 }
 /**
  * @brief free a schema
@@ -448,19 +189,6 @@ Schema *createSchema (int numAttr, char **attrNames, DataType *dataTypes, int *t
  * @return RC_OK
  */
 RC freeSchema(Schema *schema) {
-    if (schema == NULL)
-        return RC_UNVALID_HANDLE;
-    // 释放动态分配的成员（假设attrNames等是动态分配的）
-    if(schema->attrNames != NULL)
-        free(schema->attrNames);
-    if(schema->dataTypes != NULL)
-        free(schema->dataTypes);
-    if(schema->typeLength != NULL)
-        free(schema->typeLength);
-    if(schema->keyAttrs != NULL)
-        free(schema->keyAttrs);
-    // 释放schema结构体
-    free(schema);
     return RC_OK;
 }
 /**
@@ -472,22 +200,6 @@ RC freeSchema(Schema *schema) {
  */
 RC createRecord (Record **record, Schema *schema)
 {
-    if(record == NULL || schema == NULL)
-        return RC_UNVALID_HANDLE;
-    
-    *record = (Record *)malloc(sizeof(Record));
-    if(*record == NULL)
-        return RC_MEMORY_ALLOC_FAILED;
-    (*record)->data = (char *)malloc(getRecordSize(schema));
-    if((*record)->data == NULL)
-    {
-        free(*record);
-        return RC_MEMORY_ALLOC_FAILED;
-    }
-
-    (*record)->id.page = -1;
-    (*record)->id.slot = -1;
-
     return RC_OK;
 }
 /**
@@ -498,14 +210,7 @@ RC createRecord (Record **record, Schema *schema)
  */
 RC freeRecord (Record *record)
 {
-    if (record)
-    {
-        free(record->data);
-        free(record);
-        return RC_OK;
-    }
-    else
-        return RC_UNVALID_HANDLE;
+    return RC_OK;
 }
 /**
  * @brief get attribute value (simple version)
@@ -518,44 +223,6 @@ RC freeRecord (Record *record)
  */
 RC getAttr (Record *record, Schema *schema, int attrNum, Value **value)
 {
-    if(record == NULL || schema == NULL || attrNum < 0 || attrNum >= schema->numAttr || value == NULL)
-        return RC_UNVALID_HANDLE;
-    
-    *value = (Value *)malloc(sizeof(Value));
-    if(*value == NULL)
-        return RC_MEMORY_ALLOC_FAILED;
-    (*value)->dt = schema->dataTypes[attrNum];
-    char *dataPtr = record->data;
-    int offset = 0;
-    // calculate all attribute offsets
-    for(int i = 0; i < attrNum; i++)
-    {
-        switch(schema->dataTypes[i])
-        {
-            case DT_INT: offset += sizeof(int); break;
-            case DT_STRING: offset += schema->typeLength[i]; break;
-            case DT_FLOAT: offset += sizeof(float); break;
-            case DT_BOOL: offset += sizeof(bool); break;
-            default: free(*value); return RC_INVALID_PARAMS;
-        }
-    }
-    // copy the attribute value
-    switch(schema->dataTypes[attrNum])
-    {
-        case DT_INT: (*value)->v.intV = *((int *)(dataPtr + offset)); break;
-        case DT_STRING: 
-            (*value)->v.stringV = (char *)malloc(schema->typeLength[attrNum]);
-            if (((*value)->v.stringV) == NULL)
-                {
-                    free(*value);
-                    return RC_MEMORY_ALLOC_FAILED;
-                }
-            memcpy((*value)->v.stringV, (char *)(dataPtr + offset), schema->typeLength[attrNum]); 
-            break; // 根据文档要求，记录中的字符串不包含结尾的"\0"~!
-        case DT_FLOAT: (*value)->v.floatV = *((float *)(dataPtr + offset)); break;
-        case DT_BOOL: (*value)->v.boolV = *((bool *)(dataPtr + offset)); break;
-        default: free(*value); return RC_INVALID_PARAMS;
-    }
     return RC_OK;
 }
 /**
@@ -569,34 +236,5 @@ RC getAttr (Record *record, Schema *schema, int attrNum, Value **value)
  */
 RC setAttr (Record *record, Schema *schema, int attrNum, Value *value)
 {
-    if(record == NULL || schema == NULL || attrNum < 0 || attrNum >= schema->numAttr || value == NULL)
-        return RC_UNVALID_HANDLE;
-
-    char *dataPtr = record->data;
-    int offset = 0;
-    // calculate all attribute offsets
-    for(int i = 0; i < attrNum; i++)
-    {
-        switch(schema->dataTypes[i])
-        {
-            case DT_INT: offset += sizeof(int); break;
-            case DT_STRING: offset += schema->typeLength[i]; break;
-            case DT_FLOAT: offset += sizeof(float); break;
-            case DT_BOOL: offset += sizeof(bool); break;
-            default: free(value); return RC_INVALID_PARAMS;
-        }
-    }
-    // set the attribute value
-    switch(schema->dataTypes[attrNum])
-    {
-        case DT_INT: *((int *)(dataPtr + offset)) = value->v.intV; break;
-        case DT_STRING: 
-            memcpy((char *)(dataPtr + offset), value->v.stringV, schema->typeLength[attrNum]); 
-            break; // 根据文档要求，记录中的字符串不包含结尾的"\0"~!
-        case DT_FLOAT: *((float *)(dataPtr + offset)) = value->v.floatV; break;
-        case DT_BOOL: *((bool *)(dataPtr + offset)) = value->v.boolV; break;
-        default: free(value); return RC_INVALID_PARAMS;
-    }
-
     return RC_OK;
 }
